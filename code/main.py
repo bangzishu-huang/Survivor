e48a04df5ffbb1d71f8ffd618b61dca5cb27087d
bangzishu-huang <bangzishu@gmail.com>
2026-08-25 01:24:42 -0500

    basic setup and movement for player

---
+++ code/main.py
@@ -0,0 +1,34 @@
+from settings import *
+from player import Player
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Survivor')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.all_sprites = pygame.sprite.Group()
+
+        self.player = Player((400, 300), self.all_sprites)
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick() / 1000
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.all_sprites.update(dt)
+
+            self.display_surface.fill('black')
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
