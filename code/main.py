6457af2495d7f1b27e545f49c4ecc24fb8aaceb9
bangzishu-huang <bangzishu@gmail.com>
2026-08-25 16:22:12 -0500

    tweaking code and adding map tilsets

--- code/main.py
+++ code/main.py
@@ -3,6 +3,7 @@ from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
 from random import randint
+from groups import AllSprites
 
 class Game:
     def __init__(self):
@@ -12,23 +13,22 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
 
         self.setup()
 
         self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
-        for i in range(6):
-            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
-            w, h = randint(60, 100), randint(50, 100)
-            CollisionSprite((x,y), (w,h), (self.all_sprites, self.collision_sprites))
 
     def setup(self):
         map = load_pygame(join('code', 'data', 'maps', 'world.tmx'))
+
+        for x,y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE,y * TILE_SIZE), image, self.all_sprites)
         for obj in map.get_layer_by_name('Objects'):
-            print(obj.x)
-            print(obj.y)
-            print(obj.image)
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
 
     def run(self):
         while self.running:
