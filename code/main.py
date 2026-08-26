f441249633835c270bdceb6483b2fc413c2d03c3
bangzishu-huang <bangzishu@gmail.com>
2026-08-26 10:47:44 -0500

    finished basic mechanisms, working on game over screen and lives implementation

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ from settings import *
 from player import Player
 from sprites import *
 from pytmx.util_pygame import load_pygame
-from random import randint
+from random import randint, choice
 from groups import AllSprites
 
 class Game:
@@ -12,25 +12,59 @@ class Game:
         pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
-        self.load_images()
 
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         self.bullet_sprites = pygame.sprite.Group()
-
-
-        self.setup()
+        self.enemy_sprites = pygame.sprite.Group()
 
         self.can_shoot = True
         self.shoot_time = 0
-        self.gun_cooldown = 100
+        self.gun_cooldown = 200
+
+        self.enemy_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.enemy_event, 300)
+        self.spawn_positions = []
+
+        self.max_lives = 3
+        self.lives = self.max_lives
+        self.hit_time = 0
+        self.hit_cooldown = 1000
+        self.game_over = False
+        self.overlay_alpha = 0
+        self.overlay_speed = 300
+        self.overlay_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.overlay_surf.fill('black')
+        self.replay_button_rect = None
+        self.game_over_font = pygame.font.Font(None, 90)
+        self.button_font = pygame.fontFont(None, 44)
+
+        self.shoot_sound = pygame.mixer.Sound(join('code', 'audio', 'shoot.wav'))
+        self.shoot_sound.set_volume(0.4)
+        self.impact_sound = pygame.mixer.Sound(join('code', 'audio', 'impact.ogg'))
+        self.music = pygame.mixer.Sound(join('code', 'audio', 'music.wav'))
+        self.music.play(loops= -1)
+        self.music.set_volume(0.2)
+        self.load_images()
+        self.setup()
 
     def load_images(self):
         self.bullet_surf = pygame.image.load(join('code', 'images', 'weapon', 'bullet.png')).convert_alpha()
 
+        folders = list(walk(join('code', 'images', 'enemies')))[0][1]
+        self.enemy_frames = {}
+        for folder in folders:
+            for folder_path, _, file_names in walk(join('code', 'images', 'enemies', folder)):
+                self.enemy_frames[folder] = []
+                for file_name in sorted(file_names, key = lambda name: int(name.split('.')[0])):
+                    full_path = join(folder_path, file_name)
+                    surf = pygame.image.load(full_path).convert_alpha()
+                    self.enemy_frames[folder].append(surf)
+
 
     def input(self):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            self.shoot_sound.play()
             pos = self.gun.rect.center + self.gun.player_direction * 40
             Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
             print('shoot')
@@ -58,6 +92,22 @@ class Game:
             if marker.name == 'Player':
                 self.player = Player((marker.x, marker.y), self.all_sprites, self.collision_sprites)
                 self.gun = Gun(self.player, self.all_sprites)
+            else: 
+                self.spawn_positions.append((marker.x, marker.y))
+
+    def bullet_collision(self):
+        if self.bullet_sprites:
+            for bullet in self.bullet_sprites:
+                self.collision_sprites = pygame.sprite.spritecollide(bullet, self.enemy_sprites, False, pygame.sprite.collide_mask)
+                if self.collision_sprites:
+                    self.impact_sound.play()
+                    for sprite in self.collision_sprites:
+                        sprite.destroy()
+                    bullet.kill()                                  
+
+    def player_collision(self):
+        if pygame.sprite.spritecollide(self.player, self.enemy_sprites, False, pygame.sprite.collide_mask):
+            self.running = False
 
     def run(self):
         while self.running:
@@ -65,10 +115,14 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == self.enemy_event:
+                    Enemy(choice(self.spawn_positions), choice(list(self.enemy_frames.values())), (self.all_sprites, self.enemy_sprites), self.player, self.collision_sprites)
 
             self.gun_timer()
             self.input()
             self.all_sprites.update(dt)
+            self.bullet_collision()
+            self.player_collision()
 
             self.display_surface.fill('black')
             self.all_sprites.draw(self.player.rect.center)
