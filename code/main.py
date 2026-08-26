de93d78fb015a1ea79f835cd29b7753072fb7191
bangzishu-huang <bangzishu@gmail.com>
2026-08-26 11:53:09 -0500

    adding the game over and lives features

--- code/main.py
+++ code/main.py
@@ -37,7 +37,7 @@ class Game:
         self.overlay_surf.fill('black')
         self.replay_button_rect = None
         self.game_over_font = pygame.font.Font(None, 90)
-        self.button_font = pygame.fontFont(None, 44)
+        self.button_font = pygame.font.Font(None, 44)
 
         self.shoot_sound = pygame.mixer.Sound(join('code', 'audio', 'shoot.wav'))
         self.shoot_sound.set_volume(0.4)
@@ -61,6 +61,11 @@ class Game:
                     surf = pygame.image.load(full_path).convert_alpha()
                     self.enemy_frames[folder].append(surf)
 
+        heart_scale = 0.05
+        heart_raw = pygame.image.load(join('code', 'images', 'player', 'heart.png')).convert_alpha()
+        size = (int(heart_raw.get_width() * heart_scale), int(heart_raw.get_height() * heart_scale))
+        self.heart_surf = pygame.transform.smoothscale(heart_raw, size)
+        self.heart_gray_surf = pygame.transform.grayscale(self.heart_surf)
 
     def input(self):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
@@ -106,8 +111,67 @@ class Game:
                     bullet.kill()                                  
 
     def player_collision(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.hit_time < self.hit_cooldown:
+            return
         if pygame.sprite.spritecollide(self.player, self.enemy_sprites, False, pygame.sprite.collide_mask):
-            self.running = False
+            self.hit_time = current_time
+            self.lives -= 1
+            if self.lives <= 0:
+                self.trigger_game_over()
+
+    def trigger_game_over(self):
+        self.game_over = True
+        self.overlay_alpha = 0
+        self.replay_button_rect = None
+
+    def reset(self):
+        self.all_sprites.empty()
+        self.collision_sprites.empty()
+        self.bullet_sprites.empty()
+        self.spawn_positions = []
+
+        self.lives = self.max_lives
+        self.hit_time = 0
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.game_over = False
+        self.overlay_alpha = 0
+        self.replay_button_rect = None
+
+        self.setup()
+
+    def draw_heart(self):
+        spacing = 10
+        heart_w = self.heart_surf.get_width()
+        total_width = self.max_lives * heart_w + (self.max_lives - 1) * spacing
+        start_x = WINDOW_WIDTH / 2 - total_width / 2
+        y = WINDOW_HEIGHT / 2 - 150
+
+        for i in range(self.max_lives):
+            x = start_x + i * (heart_w + spacing)
+            surf = self.heart_surf if i < self.lives else self.heart_gray_surf
+            self.display_surface.blit(surf, (x, y))
+
+    def draw_game_over(self, dt):
+        self.overlay_alpha = min(255, self.overlay_alpha + self.overlay_speed * dt)
+        self.overlay_surf.set_alpha(self.overlay_alpha)
+        self.display_surface.blit(self.overlay_surf, (0, 0))
+
+        text_surf = self.game_over_font.render('GAME OVER', True, 'white')
+        text_rect = text_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 40))
+        self.display_surface.blit(text_surf, text_rect)
+
+        if self.overlay_alpha >= 255:
+            self.replay_button_rect = pygame.FRect(0, 0, 220, 60)
+            self.replay_button_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 50)
+            pygame.draw.rect(self.display_surface, 'white', self.replay_button_rect, border_radius=8)
+
+            replay_text = self.button_font.render('Play Again', True, 'black')
+            replay_text_rect = replay_text.get_frect(center=self.replay_button_rect.center)
+            self.display_surface.blit(replay_text, replay_text_rect)
+        else:
+            self.replay_button_rect = None
 
     def run(self):
         while self.running:
@@ -115,17 +179,27 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                if event.type == self.enemy_event:
+                if not self.game_over and event.type == self.enemy_event:
                     Enemy(choice(self.spawn_positions), choice(list(self.enemy_frames.values())), (self.all_sprites, self.enemy_sprites), self.player, self.collision_sprites)
 
-            self.gun_timer()
-            self.input()
-            self.all_sprites.update(dt)
-            self.bullet_collision()
-            self.player_collision()
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    if self.game_over and self.replay_button_rect and self.replay_button_rect.collidepoint(event.pos):
+                        self.reset()
+
+            if not self.game_over:
+                self.gun_timer()
+                self.input()
+                self.all_sprites.update(dt)
+                self.bullet_collision()
+                self.player_collision()
 
             self.display_surface.fill('black')
             self.all_sprites.draw(self.player.rect.center)
+            self.draw_heart()
+
+            if self.game_over:
+                self.draw_game_over(dt)
+
             pygame.display.update()
 
         pygame.quit()
@@ -136,3 +210,5 @@ class Game:
 if __name__ == '__main__':
     game = Game()
     game.run()
+
+# then afterwards i should also start to include
--- code/sprites.py
+++ code/sprites.py
@@ -32,12 +32,14 @@ class Gun(pygame.sprite.Sprite):
         self.player_direction = (mouse_position - player_position).normalize()
 
     def rotate_gun(self):
-        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
-        if self.player_direction.x > 0: 
-            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
-        else:
-            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        angle = degrees(atan2(-self.player_direction.y, self.player_direction.x))
+        self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        if self.player_direction.x < 0: 
             self.image = pygame.transform.flip(self.image, False, True)
+        #     self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+        # else:
+        #     self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        #     self.image = pygame.transform.flip(self.image, False, True)
 
     def update(self, _):
         self.get_direction()
