2f4c62998a55fce3fb85e9a2228990f59150bd96
bangzishu-huang <bangzishu@gmail.com>
2026-08-26 12:46:05 -0500

    added tips to play the game

--- code/main.py
+++ code/main.py
@@ -30,14 +30,22 @@ class Game:
         self.lives = self.max_lives
         self.hit_time = 0
         self.hit_cooldown = 1000
+        self.score = 0
+        self.high_score = 0
+        self.game_started = False
         self.game_over = False
         self.overlay_alpha = 0
         self.overlay_speed = 300
         self.overlay_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.overlay_surf.fill('black')
         self.replay_button_rect = None
-        self.game_over_font = pygame.font.Font(None, 90)
         self.button_font = pygame.font.Font(None, 44)
+        self.score_font = pygame.font.Font(None, 42)
+        self.game_over_font = pygame.font.Font(None, 90)
+        self.game_describe = pygame.font.Font(None, 30)
+
+        self.play_button_rect = pygame.FRect(0, 0, 220, 70)
+        self.play_button_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 +60)
 
         self.shoot_sound = pygame.mixer.Sound(join('code', 'audio', 'shoot.wav'))
         self.shoot_sound.set_volume(0.4)
@@ -107,7 +115,12 @@ class Game:
                 if hit_enemies:
                     self.impact_sound.play()
                     for sprite in hit_enemies:
-                        sprite.destroy()
+                        if sprite.death_time == 0:
+                            sprite.destroy()
+
+                            self.score += 1
+                            if self.score > self.high_score:
+                                self.high_score = self.score
                     bullet.kill()                                  
 
     def player_collision(self):
@@ -129,13 +142,16 @@ class Game:
         self.all_sprites.empty()
         self.collision_sprites.empty()
         self.bullet_sprites.empty()
+        self.enemy_sprites.empty()
         self.spawn_positions = []
 
         self.lives = self.max_lives
+        self.score = 0
         self.hit_time = 0
         self.can_shoot = True
         self.shoot_time = 0
         self.game_over = False
+        self.game_started = True
         self.overlay_alpha = 0
         self.replay_button_rect = None
 
@@ -153,18 +169,51 @@ class Game:
             surf = self.heart_surf if i < self.lives else self.heart_gray_surf
             self.display_surface.blit(surf, (x, y))
 
+    def draw_score(self):
+        high_score_surf = self.score_font.render(f'HIGH SCORE: {self.high_score}', True, 'white')
+        high_score_rect = high_score_surf.get_frect(center = (WINDOW_WIDTH / 2, 35))
+        score_surf = self.score_font.render(f'SCORE: {self.score}', True, 'white')
+        score_rect = score_surf.get_frect(center = (WINDOW_WIDTH / 2, 75))
+        self.display_surface.blit(high_score_surf, high_score_rect)
+        self.display_surface.blit(score_surf, score_rect)
+
+    def draw_start(self):
+        overlay = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
+        overlay.fill((0, 0, 0, 150))
+        self.display_surface.blit(overlay, (0, 0))
+        title_surf = self.game_over_font.render('SURVIVOR', True, 'white')
+        title_rect = title_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 60))
+        self.display_surface.blit(title_surf, title_rect)
+
+        describe_surf = self.game_describe.render('Use keys "WASD" to move, and mouse to aim and shoot!', True, 'white')
+        describe_rect = describe_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 2))
+        self.display_surface.blit(describe_surf, describe_rect)
+
+        pygame.draw.rect(self.display_surface, 'white', self.play_button_rect, border_radius=10)
+        play_text = self.button_font.render('PLAY', True, 'black')
+        play_text_rect = play_text.get_frect(center = self.play_button_rect.center)
+        self.display_surface.blit(play_text, play_text_rect)
+
     def draw_game_over(self, dt):
         self.overlay_alpha = min(255, self.overlay_alpha + self.overlay_speed * dt)
         self.overlay_surf.set_alpha(self.overlay_alpha)
         self.display_surface.blit(self.overlay_surf, (0, 0))
 
         text_surf = self.game_over_font.render('GAME OVER', True, 'white')
-        text_rect = text_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 40))
+        text_rect = text_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 85))
         self.display_surface.blit(text_surf, text_rect)
+
+        score_surf = self.score_font.render(f'SCORE: {self.score}', True, 'white')
+        score_rect = score_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 5))
+        self.display_surface.blit(score_surf, score_rect)
+
+        high_score_surf = self.score_font.render(f'HIGH SCORE: {self.high_score}', True, 'white')
+        high_score_rect = high_score_surf.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 35))
+        self.display_surface.blit(high_score_surf, high_score_rect)
 
         if self.overlay_alpha >= 255:
             self.replay_button_rect = pygame.FRect(0, 0, 220, 60)
-            self.replay_button_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 50)
+            self.replay_button_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 60)
             pygame.draw.rect(self.display_surface, 'white', self.replay_button_rect, border_radius=8)
 
             replay_text = self.button_font.render('Play Again', True, 'black')
@@ -179,14 +228,20 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                if not self.game_over and event.type == self.enemy_event:
+
+                if self.game_started and not self.game_over and event.type == self.enemy_event:
                     Enemy(choice(self.spawn_positions), choice(list(self.enemy_frames.values())), (self.all_sprites, self.enemy_sprites), self.player, self.collision_sprites)
 
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    if self.game_over and self.replay_button_rect and self.replay_button_rect.collidepoint(event.pos):
-                        self.reset()
+                    if not self.game_started:
+                        if self.play_button_rect.collidepoint(event.pos):
+                            self.game_started = True
 
-            if not self.game_over:
+                    elif self.game_over:
+                        if self.game_over and self.replay_button_rect and self.replay_button_rect.collidepoint(event.pos):
+                            self.reset()
+
+            if self.game_started and not self.game_over:
                 self.gun_timer()
                 self.input()
                 self.all_sprites.update(dt)
@@ -196,6 +251,12 @@ class Game:
             self.display_surface.fill('black')
             self.all_sprites.draw(self.player.rect.center)
             self.draw_heart()
+
+            if self.game_started:
+                self.draw_score()
+
+            if not self.game_started:
+                self.draw_start()
 
             if self.game_over:
                 self.draw_game_over(dt)
@@ -211,4 +272,3 @@ if __name__ == '__main__':
     game = Game()
     game.run()
 
-# then afterwards i should also start to include
