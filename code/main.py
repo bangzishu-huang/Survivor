0afb12a06864b14e33ceec867d0aead82a9f93af
bangzishu-huang <bangzishu@gmail.com>
2026-08-26 08:58:27 -0500

    setting up weapon function

--- code/main.py
+++ code/main.py
@@ -12,12 +12,38 @@ class Game:
         pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_images()
 
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
 
 
         self.setup()
+
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('code', 'images', 'weapon', 'bullet.png')).convert_alpha()
+
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 40
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            print('shoot')
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
+
 
     def setup(self):
         map = load_pygame(join('code', 'data', 'maps', 'world.tmx'))
@@ -31,6 +57,7 @@ class Game:
         for marker in map.get_layer_by_name('Entities'):
             if marker.name == 'Player':
                 self.player = Player((marker.x, marker.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
     def run(self):
         while self.running:
@@ -39,6 +66,8 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             self.display_surface.fill('black')
--- code/sprites.py
+++ code/sprites.py
@@ -1,4 +1,5 @@
 from settings import *
+from math import atan2, degrees
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -13,3 +14,39 @@ class CollisionSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1,0)
+
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('code', 'images', 'weapon', 'gun.png'))
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
+
+    def get_direction(self):
+        mouse_position = pygame.Vector2(pygame.mouse.get_pos())
+        player_position = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player_direction = (mouse_position - player_position).normalize()
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0: 
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, direction, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = pos)
