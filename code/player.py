fb3af9ce8403d5082ca982b9978c06e0fedafc5b
bangzishu-huang <bangzishu@gmail.com>
2026-08-25 23:44:36 -0500

    player movement

--- code/player.py
+++ code/player.py
@@ -4,6 +4,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.load_images()
+        self.state, self.frame_index = 'right', 0
         self.image = pygame.image.load(join('code', 'images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center = pos)
         self.hitbox_rect = self.rect.inflate(-60, -90)
@@ -16,7 +17,13 @@ class Player(pygame.sprite.Sprite):
     def load_images(self):
         self.frames = {'left': [], 'right': [], 'up': [], 'down': []}
 
-        print(walk(join('code', 'images', 'player')))
+        for state in self.frames.keys():
+            for folder_path, sub_folder, file_names in walk(join('code', 'images', 'player', state)):
+                if file_names:
+                    for file_name in sorted(file_names, key= lambda name: int(name.split('.')[0])): 
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -43,6 +50,17 @@ class Player(pygame.sprite.Sprite):
                 else:
                     if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
                     if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+
+    def animate(self, dt):
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+        if self.direction.y != 0:
+            self.state ='down' if self.direction.y > 0 else 'up'
+
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0 
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
